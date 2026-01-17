87378b5384eafd4ace2b9a45e96a300175667483
saitama5646 <hisogonfrisk@gmail.com>
2026-01-16 19:16:17 -0500

    Expand WORDS dictionary with various categories

--- words.py
+++ words.py
@@ -1,1 +1,80 @@
-WORDS = {"general": ["Pizza","Playa","Hospital","Computadora"]}
+WORDS = {
+
+"objetos": [
+"Celular","Computadora","Teclado","Mouse","Pantalla","Televisión","Control",
+"Auriculares","Parlante","Micrófono","Cámara","Reloj","Pulsera","Anillo",
+"Lámpara","Mesa","Silla","Sofá","Cama","Almohada","Cobija","Ventilador",
+"Aire acondicionado","Refrigerador","Microondas","Horno","Licuadora",
+"Cafetera","Tostadora","Plancha","Lavadora","Secadora","Escoba","Trapeador",
+"Cubo","Plato","Vaso","Taza","Cuchara","Tenedor","Cuchillo","Olla","Sartén",
+"Botella","Mochila","Bolso","Maleta","Cuaderno","Libro","Lapicero","Lápiz",
+"Borrador","Regla","Tijeras","Pegamento","Papel","Carpeta","Archivador",
+"Cargador","Batería","Cable","Enchufe","Extensión","Router","Modem",
+"Impresora","Escáner","USB","Disco duro","SSD","Memoria","Tarjeta gráfica",
+"Procesador","Placa madre","Fuente","Gabinete","Joystick","Consola",
+"Smartwatch","Tablet","Drone","Proyector","Calculadora","Candado",
+"Linterna","Martillo","Destornillador","Clavo","Tornillo","Taladro",
+"Serrucho","Llave inglesa","Escalera","Cinta métrica","Nivel"
+],
+
+"lugares": [
+"Casa","Departamento","Edificio","Escuela","Universidad","Instituto",
+"Hospital","Clínica","Consultorio","Farmacia","Supermercado","Tienda",
+"Mercado","Centro comercial","Cine","Teatro","Biblioteca","Museo",
+"Parque","Plaza","Jardín","Bosque","Selva","Playa","Isla","Puerto",
+"Aeropuerto","Avión","Terminal","Estación","Metro","Autobús",
+"Carretera","Puente","Túnel","Hotel","Hostal","Motel","Restaurante",
+"Cafetería","Bar","Discoteca","Gimnasio","Estadio","Cancha","Piscina",
+"Playa","Río","Lago","Cascada","Montaña","Volcán","Desierto","Cueva",
+"Castillo","Palacio","Iglesia","Catedral","Templo","Monasterio",
+"Oficina","Banco","Notaría","Municipalidad","Comisaría","Prisión",
+"Cuartel","Base militar","Laboratorio","Fábrica","Almacén","Bodega",
+"Puerto","Astillero","Granja","Rancho","Establo","Zoológico","Acuario",
+"Planetario","Observatorio","Estudio","Set de grabación"
+],
+
+"personas": [
+"Doctor","Enfermero","Cirujano","Dentista","Psicólogo","Psiquiatra",
+"Profesor","Maestro","Alumno","Estudiante","Director","Rector",
+"Abogado","Juez","Fiscal","Notario","Policía","Detective","Agente",
+"Militar","Soldado","General","Capitán","Bombero","Rescatista",
+"Paramédico","Piloto","Copiloto","Azafata","Ingeniero","Arquitecto",
+"Diseñador","Programador","Desarrollador","Tester","Analista",
+"Hacker","Streamer","Youtuber","Influencer","Periodista","Reportero",
+"Escritor","Poeta","Novelista","Editor","Traductor","Actor","Actriz",
+"Director","Productor","Guionista","Camarógrafo","Fotógrafo",
+"Músico","Cantante","Guitarrista","Bajista","Baterista","Pianista",
+"DJ","Compositor","Productor musical","Rapero","Violinista",
+"Pintor","Escultor","Ilustrador","Animador","Chef","Cocinero",
+"Pastelero","Panadero","Mesero","Bartender","Sommelier",
+"Contador","Economista","Administrador","Empresario","Vendedor",
+"Comerciante","Cajero","Recepcionista","Secretario","Asistente",
+"Gerente","Supervisor","Director ejecutivo"
+],
+
+"acciones": [
+"Correr","Caminar","Saltar","Bailar","Cantar","Gritar","Susurrar",
+"Hablar","Escuchar","Mirar","Observar","Pensar","Imaginar","Soñar",
+"Escribir","Leer","Dibujar","Pintar","Borrar","Copiar","Pegar",
+"Cortar","Pegar","Construir","Romper","Armar","Desarmar","Reparar",
+"Programar","Depurar","Compilar","Diseñar","Planear","Organizar",
+"Comprar","Vender","Negociar","Pagar","Cobrar","Ahorrar","Invertir",
+"Viajar","Explorar","Descubrir","Buscar","Encontrar","Perder",
+"Ganar","Competir","Entrenar","Practicar","Jugar","Enseñar",
+"Aprender","Estudiar","Explicar","Evaluar","Analizar","Decidir",
+"Elegir","Adivinar","Mentir","Engañar","Confiar","Sospechar",
+"Investigar","Acusar","Defender","Proteger","Atacar","Escapar"
+],
+
+"abstractos": [
+"Tiempo","Espacio","Velocidad","Distancia","Fuerza","Energía",
+"Gravedad","Materia","Átomo","Partícula","Universo","Galaxia",
+"Destino","Suerte","Azar","Caos","Orden","Equilibrio","Poder",
+"Control","Libertad","Justicia","Verdad","Mentira","Miedo","Valor",
+"Amor","Odio","Deseo","Ambición","Orgullo","Ego","Honra","Respeto",
+"Confianza","Duda","Fe","Esperanza","Tristeza","Alegría","Felicidad",
+"Ansiedad","Estrés","Calma","Paz","Silencio","Ruido","Ideas",
+"Pensamiento","Recuerdo","Memoria","Olvido","Conciencia","Identidad",
+"Personalidad","Carácter","Destino","Propósito","Sentido","Caos"
+]
+}
